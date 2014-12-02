7870001f17bddd89e06794d224f94a8bac3069e2
RockyPan <pankkk@hotmail.com>
2014-12-02 10:53:41 +0800

    Initial Commit

---
+++ ListeningTool/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ListeningTool
+//
+//  Created by PanKyle on 14/12/2.
+//  Copyright (c) 2014年 TGD. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ListeningTool/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ListeningTool
+//
+//  Created by PanKyle on 14/12/2.
+//  Copyright (c) 2014年 TGD. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
